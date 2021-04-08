3b3425cd711fc087eada931cd6818b7eafc4854c
VictorAttar <vsla@cin.ufpe.br>
2021-04-08 01:07:00 -0300

    ajustes eslint

--- src/Pages/UserDebtsList/Components/DebtsTable.tsx
+++ src/Pages/UserDebtsList/Components/DebtsTable.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import Table from "Components/Custom/Table";
 import { Grid } from "@material-ui/core";
